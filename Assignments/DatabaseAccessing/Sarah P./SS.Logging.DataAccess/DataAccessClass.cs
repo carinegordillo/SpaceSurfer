8762b19cc5479b11ca569346e0b588bd2aca761c
SarahP15 <80946504+SarahP15@users.noreply.github.com>
2023-10-11 04:18:29 +0000

    Sarahs assignment

---
+++ Assignments/DatabaseAccessing/Sarah P./SS.Logging.DataAccess/DataAccessClass.cs
@@ -0,0 +1,42 @@
+﻿namespace SS.Logging.DataAccess
+{
+    using System;
+    using System.Data.SqlClient;
+
+    public class DataAccessClass
+    {
+
+        string connectionString = @"Data Source=localhost\SpaceSurfer;Initial Catalog=SS_Server;User Id=sa;Password=r@ysbb@ll2013;";
+
+        public void SaveData(string username, string password, string timestamp)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    string sql = "INSERT INTO LogsLogin (Username, Password, Timestamp) VALUES (@Username, @Password, @Timestamp)";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        // Parameterized query to prevent SQL injection
+                        command.Parameters.AddWithValue("@Username", username);
+                        command.Parameters.AddWithValue("@Password", password);
+                        command.Parameters.AddWithValue("@Timestamp", timestamp);
+
+                        command.ExecuteNonQuery(); // Execute the query
+                    }
+
+                    Console.WriteLine("Data saved successfully.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error saving data: {ex.Message}");
+                    // Handle the exception (log, throw, or handle as appropriate for your application)
+                }
+            }
+        }
+    }
+
+}
