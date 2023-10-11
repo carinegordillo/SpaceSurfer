785399c3c051684d067b8455024e581d80340381
carinegordillo <carinegordillo@gmail.com>
2023-10-10 21:23:57 -0700

    carine's assignment

---
+++ Assignments/DatabaseAccessing/Carine G./Logging/SS.Logger.cs
@@ -0,0 +1,24 @@
+﻿
+
+using SS.Logging.DataAccessing;
+
+namespace SS.Logger
+{
+    public class Logger
+    {
+        private readonly DataAccess _dataAccess;
+
+        public Logger()
+        {
+            _dataAccess = new DataAccess();
+        }
+
+        /// 
+        public void Log(string name, string pass, DateTime currTime)
+        {
+            Console.WriteLine($"Username: {name}\nPassword: {pass}\nTime: {currTime}");
+
+            _dataAccess.SaveData(name, pass, currTime);
+        }
+    }
+};
