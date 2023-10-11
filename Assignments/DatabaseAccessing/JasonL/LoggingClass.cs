f11f33de4be91aea53a58838c6f331bf41791629
Jayyybbl <81000907+Jayyybbl@users.noreply.github.com>
2023-10-11 00:17:46 -0700

    Add files via upload

---
+++ Assignments/DatabaseAccessing/JasonL/LoggingClass.cs
@@ -0,0 +1,21 @@
+﻿using SS.Logging.DataAccessing;
+
+namespace SS.Logging
+{
+    public class LoggingClass
+    {
+        private readonly DataAccessor _dataAccess;
+
+        public LoggingClass()
+        {
+            _dataAccess = new DataAccessor();
+        }
+
+        public void Log(string userName, string passWord, DateTime currentTime)
+        {
+            Console.WriteLine($"Logging: {userName} {passWord} {currentTime}");
+
+            _dataAccess.SaveData(userName, passWord, currentTime);
+        }
+    }
+};
