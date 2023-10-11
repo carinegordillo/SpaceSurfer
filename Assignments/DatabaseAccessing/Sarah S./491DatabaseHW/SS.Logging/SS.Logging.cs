1451da63be1d1ad20564cacbea90da6511d8899b
sanarahtos <4sarahsantos@gmail.com>
2023-10-10 21:32:43 -0700

    added my database hw

---
+++ Assignments/DatabaseAccessing/Sarah S./491DatabaseHW/SS.Logging/SS.Logging.cs
@@ -0,0 +1,22 @@
+﻿using SS.Logging.DataAccessing;
+
+namespace SS.Logging
+{
+    public class Logger
+    {
+        // Set the data 
+        private readonly DataAccess _dataAccess;
+
+        public Logger()
+        {
+            _dataAccess = new DataAccess();
+        }
+
+        // log user inputs
+        public void Log(string uName, string pw, DateTime timeNow)
+        {
+            Console.WriteLine($"Username: {uName}\nPassword: {pw}\nTime: {timeNow}");
+            _dataAccess.SaveData(uName, pw, timeNow);
+        }
+    }
+};
