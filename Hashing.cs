5b225e905f99a71d709f71ccf5b2defaa4118480
SarahP15 <80946504+SarahP15@users.noreply.github.com>
2023-12-03 19:50:13 -0800

    added authn (not finished) and hashing.cs

---
+++ Hashing.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+
+namespace SS.Backend.SharedNamespace
+{
+    public class Hashing
+    {
+        public byte[] PBKDF2(byte[] data, byte[] spice, int iteration, int length)
+        {
+            using (var key = new Rfc2898DeriveBytes(data, spice, iteration))
+            {
+                return key.GetBytes(length);
+            }
+        }
+
+        public byte[] HMAC256(byte[] data, byte[] key)
+        {
+            using (var hmac = new HMACSHA256(key))
+            {
+                return hmac.ComputeHash(data);
+            }
+        }
+    }
+}
---
+++ SS.Backend.Security.AuthN/IAuthN.cs
@@ -0,0 +1,15 @@
+﻿namespace SS.Backend.Security.AuthN
+{
+    public interface IAuthN
+    {
+
+        public byte[] genOTP();
+
+        public bool compare(byte[] user_hash, byte[] db_hash);
+
+        public string byteArrayToString(byte[] arr);
+
+        public byte[] hashOTP(byte[] otp);
+
+    }
+}
