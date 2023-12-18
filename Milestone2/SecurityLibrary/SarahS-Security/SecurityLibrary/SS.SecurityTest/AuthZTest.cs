699437dea105af26785501a0bfb80e73443de732
sanarahtos <4sarahsantos@gmail.com>
2023-12-17 19:44:30 -0800

    IsAuthorize fail test

--- Milestone2/SecurityLibrary/SarahS-Security/SecurityLibrary/SS.SecurityTest/AuthZTest.cs
+++ Milestone2/SecurityLibrary/SarahS-Security/SecurityLibrary/SS.SecurityTest/AuthZTest.cs
@@ -35,9 +35,38 @@ public class AuthZTest
         Assert.IsTrue(result, "The user has all the required claims, so IsAuthorize should return true.");
     }
 
+    [TestMethod]
     //isAuthorize - Fail (false)
+    public void IsAuthorize_FailedRequiredClaims()
+    {
+        // Arrange
+        var authService = new SSAuthService();
 
+        var currentPrincipal = new SSPrincipal
+        {
+            UserIdentity = "Jane",
+            Claims = new Dictionary<string, string>
+            {
+                {"Role", "User"},
+                {"Department", "HR"}
+            },
+            SecurityContext = "custom"
+        };
 
+        var requiredClaims = new Dictionary<string, string>
+        {
+            {"Role", "Admin"},
+            {"Department", "IT"}
+        };
+
+        // Act
+        bool result = authService.IsAuthorize(currentPrincipal, requiredClaims);
+
+        // Assert
+        Assert.IsFalse(result, "The user is missing a required claim, so IsAuthorize should return false.");
+    }
+
+/*
     [TestMethod]
     //HasClaim - Success (true)
     public void HasClaim_Pass()
@@ -62,4 +91,5 @@ public class AuthZTest
     }
     
     //HasClaim - Fail (false)
+    */
 }
