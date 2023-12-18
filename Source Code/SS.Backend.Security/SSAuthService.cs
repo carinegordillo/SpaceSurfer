77ab2d3268fbc73ebb19caa661460b11f12946f2
sanarahtos <4sarahsantos@gmail.com>
2023-12-17 21:39:18 -0800

    more security library updates

--- Source Code/SS.Backend.Security/SSAuthService.cs
+++ Source Code/SS.Backend.Security/SSAuthService.cs
@@ -1,272 +1,212 @@
-using System.Security.Cryptography;
-using System.Text;
-using Microsoft.VisualBasic;
 using SS.Backend.DataAccess;
 using SS.Backend.SharedNamespace;
-using System.Security.Principal;
 
-namespace SS.Backend.Security;
-public class SSAuthService : IAuthenticator, IAuthorizer
+namespace SS.Backend.Security
 {
-	// authenticate method from lecture
-	
-	/*
-    public SSPrincipal Authenticate(AuthenticationRequest authRequest)
-	{
-		// validate arguments “Early Exit” (less cpu, ram = faster system)
-		// parameter vs argument (parameter = method signature, argument = actual value)
-		#region Validate arguments
-		if(authRequest is null)
-		{
-			throw new ArgumentNullException(nameof(authRequest)); 
-		}
-
-		if(String.IsNullOrWhiteSpace(authRequest.UserIdentity))
-		{
-			throw new ArgumentException($"{nameof(authRequest.UserIdentity)} must be valid");
-		}
-
-		if(String.IsNullOrWhiteSpace(authRequest.Proof))
-		{
-			throw new ArgumentException($"{nameof(authRequest.Proof)} must be valid");
-		}
-		#endregion
-
-		SSPrincipal ssPrincipal = null;
-
-		try
-		{
-			// Step1: validate with request
-			// Step2: Populate app principal object
-			
-			var claims = new Dictionary<string, string>();
-				
-		    ssPrincipal = new SSPrincipal()
-			{
-				UserIdentity = authRequest.UserIdentity,
-				Claims = claims
-			};
-		}
-
-		catch (Exception ex)
-		{
-			var errorMessage = ex.GetBaseException().Message;
-			// logging errorMessage; asynchronous
-		}
-
-		return ssPrincipal;
-	}
-	*/
-	
-    private readonly SqlDAO sqldao;
-    /*
-    private readonly GenOTP genotp;
-    private readonly Hashing hasher;
-
-    public SSAuthService(GenOTP genotp, Hashing hasher, SqlDAO sqldao)
+    public class SSAuthService : IAuthenticator, IAuthorizer
     {
-        this.genotp = genotp;
-        this.hasher = hasher;
-        this.sqldao = sqldao;
-    }
-    public async Task<(string otp, Response res)> SendOTP_and_SaveToDB(AuthenticationRequest authRequest)
-    {
-        var builder = new CustomSqlCommandBuilder();
-        Response result = new Response();
-        string user = authRequest.UserIdentity;
+        private readonly GenOTP genotp;
+        private readonly Hashing hasher;
+        private readonly SqlDAO sqldao;
 
-        try
+        public SSAuthService(GenOTP genotp, Hashing hasher, SqlDAO sqldao)
         {
-            // check if user is registered, ie if user has a role
-            var selectCommand = builder
-                .BeginSelectAll()
-                .From("UserRoles")
-                .Where($"Username = '{user}'")
-                .Build();
-            result = await sqldao.ReadSqlResult(selectCommand).ConfigureAwait(false);
-            if (result.ValuesRead == null || result.ValuesRead.Count == 0)
+            this.genotp = genotp;
+            this.hasher = hasher;
+            this.sqldao = sqldao;
+        }
+        public async Task<(string otp, Response res)> SendOTP_and_SaveToDB(AuthenticationRequest authRequest)
+        {
+            var builder = new CustomSqlCommandBuilder();
+            Response result = new Response();
+            string user = authRequest.UserIdentity;
+
+            try
             {
-                result.HasError = true;
-                result.ErrorMessage = "User does not exist.";
-                return (null, result);
-            }
-
-            // generate the otp and salt
-            string otp = genotp.generateOTP();
-            string salt = hasher.GenSalt();
-
-            // hash the otp
-            string hashedOTP = hasher.HashData(otp, salt);
-
-            // check if username already tried authenticating before, ie username exists in otp table
-            var otpRead = builder
-                .BeginSelectAll()
-                .From("OTP")
-                .Where($"Username = '{user}'")
-                .Build();
-            result = await sqldao.ReadSqlResult(otpRead).ConfigureAwait(false);
-            if (result.ValuesRead == null || result.ValuesRead.Count == 0)
-            {
-                // create and execute the sql command to insert the username, otp, salt, and timestamp to the DB
-                var parameters = new Dictionary<string, object>
-                {
-                    { "OTP", hashedOTP },
-                    { "Salt", salt },
-                    { "Timestamp", DateTime.UtcNow },
-                    { "Username", user }
-                };
-                var insertCommand = builder
-                    .BeginInsert("OTP")
-                    .Columns(parameters.Keys)
-                    .Values(parameters.Keys)
-                    .AddParameters(parameters)
+                // check if user is registered, ie if user has a role
+                var selectCommand = builder
+                    .BeginSelectAll()
+                    .From("UserRoles")
+                    .Where($"Username = '{user}'")
                     .Build();
-                result = await sqldao.SqlRowsAffected(insertCommand).ConfigureAwait(false);
-
-                // user and otp is returned so it can be used by the manager to send the otp to the user
-                return (otp, result);
-            }
-            else
-            {
-                var parameters = new Dictionary<string, object>
-                {
-                    { "OTP", hashedOTP },
-                    { "Salt", salt },
-                    { "Timestamp", DateTime.UtcNow },
-                    { "Username", user }
-                };
-                var updateCommand = builder
-                    .BeginUpdate("OTP")
-                    .Set(parameters)
-                    .Where("Username = @username")
-                    .AddParameters(parameters)
-                    .Build();
-
-                //update the otp and salt in table for that user
-                result = await sqldao.SqlRowsAffected(updateCommand).ConfigureAwait(false);
-
-                // user and otp is returned so it can be used by the manager to send the otp to the user
-                return (otp, result);
-            }
-        }
-        catch (Exception ex)
-        {
-            result.HasError = true;
-            result.ErrorMessage = ex.Message;
-            return (null, result);
-        }
-    }
-    public async Task<(SSPrincipal principal, Response res)> Authenticate(AuthenticationRequest authRequest)
-    {
-        var builder = new CustomSqlCommandBuilder();
-        Response result = new Response();
-
-        #region Validate arguments
-        if (authRequest is null)
-        {
-            throw new ArgumentNullException(nameof(authRequest));
-        }
-
-        if (System.String.IsNullOrWhiteSpace(authRequest.UserIdentity))
-        {
-            throw new ArgumentException($"{nameof(authRequest.UserIdentity)} must be valid");
-        }
-
-        if (System.String.IsNullOrWhiteSpace(authRequest.Proof))
-        {
-            throw new ArgumentException($"{nameof(authRequest.UserIdentity)} must be valid");
-        }
-        #endregion
-
-        string hashedUsername = authRequest.UserIdentity;
-        string proof = authRequest.Proof;
-
-        try
-        {
-            // create and execute sql command to read the hashedOTP from the DB
-            var selectCommand = builder
-                .BeginSelectAll()
-                .From("OTP")
-                .Where($"Username = '{hashedUsername}'")
-                .Build();
-            result = await sqldao.ReadSqlResult(selectCommand).ConfigureAwait(false);
-            string dbOTP = (string)result.ValuesRead[0][1];
-            string dbSalt = (string)result.ValuesRead[0][2];
-            DateTime timestamp = (DateTime)result.ValuesRead[0][3];
-            TimeSpan timeElapsed = DateTime.UtcNow - timestamp;
-
-            // compare the otp stored in DB with user inputted otp
-            string HashedProof = hasher.HashData(proof, dbSalt);
-            if (dbOTP == HashedProof)
-            {
-                if (timeElapsed.TotalMinutes > 2)
+                result = await sqldao.ReadSqlResult(selectCommand).ConfigureAwait(false);
+                if (result.ValuesRead == null || result.ValuesRead.Count == 0)
                 {
                     result.HasError = true;
-                    result.ErrorMessage = "OTP has expired.";
+                    result.ErrorMessage = "User does not exist.";
                     return (null, result);
+                }
+
+                // generate the otp and salt
+                string otp = genotp.generateOTP();
+                string salt = hasher.GenSalt();
+
+                // hash the otp
+                string hashedOTP = hasher.HashData(otp, salt);
+
+                // check if username already tried authenticating before, ie username exists in otp table
+                var otpRead = builder
+                    .BeginSelectAll()
+                    .From("OTP")
+                    .Where($"Username = '{user}'")
+                    .Build();
+                result = await sqldao.ReadSqlResult(otpRead).ConfigureAwait(false);
+                if (result.ValuesRead == null || result.ValuesRead.Count == 0)
+                {
+                    // create and execute the sql command to insert the username, otp, salt, and timestamp to the DB
+                    var parameters = new Dictionary<string, object>
+                    {
+                        { "OTP", hashedOTP },
+                        { "Salt", salt },
+                        { "Timestamp", DateTime.UtcNow },
+                        { "Username", user }
+                    };
+                    var insertCommand = builder
+                        .BeginInsert("OTP")
+                        .Columns(parameters.Keys)
+                        .Values(parameters.Keys)
+                        .AddParameters(parameters)
+                        .Build();
+                    result = await sqldao.SqlRowsAffected(insertCommand).ConfigureAwait(false);
+
+                    // user and otp is returned so it can be used by the manager to send the otp to the user
+                    return (otp, result);
                 }
                 else
                 {
-                    // they match and not expired, so get the roles from the DB for that user
-                    var readRoles = builder
-                        .BeginSelectAll()
-                        .From("UserRoles")
-                        .Where($"Username = '{hashedUsername}'")
+                    var parameters = new Dictionary<string, object>
+                    {
+                        { "OTP", hashedOTP },
+                        { "Salt", salt },
+                        { "Timestamp", DateTime.UtcNow },
+                        { "Username", user }
+                    };
+                    var updateCommand = builder
+                        .BeginUpdate("OTP")
+                        .Set(parameters)
+                        .Where("Username = @username")
+                        .AddParameters(parameters)
                         .Build();
-                    result = await sqldao.ReadSqlResult(readRoles).ConfigureAwait(false);
-                    if (result.ValuesRead.Count > 0)
-                    {
-                        string roles = (string)result.ValuesRead[0][1];
 
-                        // populate the principal
-                        SSPrincipal principal = new SSPrincipal();
-                        principal.UserIdentity = hashedUsername;
-                        principal.Claims.Add("Roles", roles);
+                    //update the otp and salt in table for that user
+                    result = await sqldao.SqlRowsAffected(updateCommand).ConfigureAwait(false);
 
-                        result.HasError = false;
-
-                        return (principal, result);
-                    }
-                    else
-                    {
-                        result.HasError = true;
-                        result.ErrorMessage = "No roles found for the user.";
-                        return (null, result);
-                    }
+                    // user and otp is returned so it can be used by the manager to send the otp to the user
+                    return (otp, result);
                 }
-
             }
-            else
+            catch (Exception ex)
             {
                 result.HasError = true;
-                result.ErrorMessage = "Failed to authenticate.";
+                result.ErrorMessage = ex.Message;
                 return (null, result);
             }
         }
-        catch (Exception ex)
+        public async Task<(SSPrincipal principal, Response res)> Authenticate(AuthenticationRequest authRequest)
         {
-            result.ErrorMessage = ex.Message;
-            return (null, result);
+            var builder = new CustomSqlCommandBuilder();
+            Response result = new Response();
+
+            #region Validate arguments
+            if (authRequest is null)
+            {
+                throw new ArgumentNullException(nameof(authRequest));
+            }
+
+            if (System.String.IsNullOrWhiteSpace(authRequest.UserIdentity))
+            {
+                throw new ArgumentException($"{nameof(authRequest.UserIdentity)} must be valid");
+            }
+
+            if (System.String.IsNullOrWhiteSpace(authRequest.Proof))
+            {
+                throw new ArgumentException($"{nameof(authRequest.UserIdentity)} must be valid");
+            }
+            #endregion
+
+            string hashedUsername = authRequest.UserIdentity;
+            string proof = authRequest.Proof;
+
+            try
+            {
+                // create and execute sql command to read the hashedOTP from the DB
+                var selectCommand = builder
+                    .BeginSelectAll()
+                    .From("OTP")
+                    .Where($"Username = '{hashedUsername}'")
+                    .Build();
+                result = await sqldao.ReadSqlResult(selectCommand).ConfigureAwait(false);
+                string dbOTP = (string)result.ValuesRead[0][1];
+                string dbSalt = (string)result.ValuesRead[0][2];
+                DateTime timestamp = (DateTime)result.ValuesRead[0][3];
+                TimeSpan timeElapsed = DateTime.UtcNow - timestamp;
+
+                // compare the otp stored in DB with user inputted otp
+                string HashedProof = hasher.HashData(proof, dbSalt);
+                if (dbOTP == HashedProof)
+                {
+                    if (timeElapsed.TotalMinutes > 2)
+                    {
+                        result.HasError = true;
+                        result.ErrorMessage = "OTP has expired.";
+                        return (null, result);
+                    }
+                    else
+                    {
+                        // they match and not expired, so get the roles from the DB for that user
+                        var readRoles = builder
+                            .BeginSelectAll()
+                            .From("UserRoles")
+                            .Where($"Username = '{hashedUsername}'")
+                            .Build();
+                        result = await sqldao.ReadSqlResult(readRoles).ConfigureAwait(false);
+                        if (result.ValuesRead.Count > 0)
+                        {
+                            string role = (string)result.ValuesRead[0][1];
+
+                            // populate the principal
+                            SSPrincipal principal = new SSPrincipal();
+                            principal.UserIdentity = hashedUsername;
+                            principal.Claims.Add("Role", role);
+
+                            result.HasError = false;
+
+                            return (principal, result);
+                        }
+                        else
+                        {
+                            result.HasError = true;
+                            result.ErrorMessage = "No roles found for the user.";
+                            return (null, result);
+                        }
+                    }
+
+                }
+                else
+                {
+                    result.HasError = true;
+                    result.ErrorMessage = "Failed to authenticate.";
+                    return (null, result);
+                }
+            }
+            catch (Exception ex)
+            {
+                result.ErrorMessage = ex.Message;
+                return (null, result);
+            }
+
         }
+        public async Task<bool> IsAuthorize(SSPrincipal currentPrincipal, IDictionary<string, string> requiredClaims)
+        {
+            foreach (var claim in requiredClaims)
+            {
+                if (!currentPrincipal.Claims.Contains(claim))
+                {
+                    return false;
+                }
+            }
+            return true;
 
+        }
     }
-
-	public async Task<bool> IsAuthorize(SSPrincipal currentPrincipal, IDictionary<string, string> requiredClaims)
-	{
-		foreach(var claim in requiredClaims)	
-		{
-			if(!currentPrincipal.HasClaim(requiredClaim.Key, requiredClaim.Value))
-			{
-				return false;
-			}
-		}
-		return true;
-			
-	}
 }
-
-
-// no AuthZ response
-//Dictionary<string, string>() {new (“RoleName”, “Admin”)}
-//key - RoleName, value - Admin
-// SSAuthService.IsAuthorize(currentPrincipal, new Dictionary<string, string>() {new (Key: “RoleName”,  Value: “Normal”) })
