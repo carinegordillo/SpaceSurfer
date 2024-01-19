4787de3e16caa5a81136b748dd923a9300d1adf6
carinegordillo <carinegordillo@gmail.com>
2024-01-18 21:34:09 -0800

    moved source code and added fornt/backend folder

---
+++ SourceCode/SS.Backend/SS.Backend.Security/IAuthenticator.cs
@@ -0,0 +1,10 @@
+﻿using SS.Backend.SharedNamespace;
+
+namespace SS.Backend.Security
+{
+    public interface IAuthenticator
+    {
+        Task<(SSPrincipal principal, Response res)> Authenticate(AuthenticationRequest authRequest);
+
+    }
+}
---
+++ SourceCode/SS.Backend/SS.Backend.SharedNamespace/UserPepper.cs
@@ -0,0 +1,8 @@
+namespace SS.Backend.SharedNamespace
+{
+    public class UserPepper : IUserPepper
+    {
+        public string hashedUsername{ get; set; }
+
+    }
+}
