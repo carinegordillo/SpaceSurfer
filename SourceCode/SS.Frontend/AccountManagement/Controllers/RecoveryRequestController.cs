ffe3481dcbd3e193b2229e0b2efafcf54aff7e1a
carinegordillo <carinegordillo@gmail.com>
2024-02-10 01:29:57 -0800

    startign account recovery MS

---
+++ SourceCode/SS.Frontend/AccountManagement/Controllers/RecoveryRequestController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountManagement.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class RecoverRequestController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<RecoverRequestController> _logger;
+
+    public RecoverRequestController(ILogger<RecoverRequestController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
---
+++ SourceCode/SS.Frontend/AccountManagement/Models/RequestsModel.cs
@@ -0,0 +1,8 @@
+namespace AccountManagement.Models
+{
+    public class RequestModel
+    {
+        public string userID {get;set;} 
+        public string accountStatus {get;set;} 
+    }
+}
