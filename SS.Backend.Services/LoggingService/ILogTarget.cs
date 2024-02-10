2fe5d29879612ae2b519f4e22a00ae7b62e06c79
SarahP15 <80946504+SarahP15@users.noreply.github.com>
2024-02-10 00:54:01 -0800

    updated branch with fixed milestone code

---
+++ SS.Backend.Services/LoggingService/ILogTarget.cs
@@ -0,0 +1,11 @@
+﻿
+using SS.Backend.SharedNamespace;
+
+namespace SS.Backend.Services.LoggingService
+{
+    public interface ILogTarget
+    {
+        public Task<Response> WriteData(LogEntry log);
+
+    }
+}
