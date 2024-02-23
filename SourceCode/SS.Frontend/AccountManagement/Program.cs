14e48ec15e4c18dee5f7cb742252747ecb1cd125
carinegordillo <carinegordillo@gmail.com>
2024-02-23 14:00:06 -0800

    added service as transient

--- SourceCode/SS.Frontend/AccountManagement/Program.cs
+++ SourceCode/SS.Frontend/AccountManagement/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 // Add services to the container.
-builder.Services.AddScoped<IAccountRecovery, AccountRecoveryNoInj>();
+builder.Services.AddTransient<IAccountRecovery, AccountRecoveryNoInj>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
