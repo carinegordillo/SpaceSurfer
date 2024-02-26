b11f64756caba1c688af7df42472a2a9d6eb023a
kaykayale <kaykayale@hotmail.com>
2024-02-25 18:39:01 -0800

    Registration Controller

---
+++ SourceCode/SS.Frontend/Registration/Program.cs
@@ -0,0 +1,51 @@
+// var builder = WebApplication.CreateBuilder(args);
+
+// // Add services to the container.
+
+// builder.Services.AddControllers();
+// // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
+
+// var app = builder.Build();
+
+// // Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+// app.UseHttpsRedirection();
+
+// app.UseAuthorization();
+
+// app.MapControllers();
+
+// app.Run();
+
+using SS.Backend.Services
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+
+// Add services to the container.
+builder.Services.AddTransient<IAccountCreation, AccountCreation>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
