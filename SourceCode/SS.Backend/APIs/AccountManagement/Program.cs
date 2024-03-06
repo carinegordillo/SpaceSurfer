843f3945aaac8f03ce156e322dcd52f3691ca3cc
Galich6 <brandon.galich01@student.csulb.edu>
2024-03-05 16:51:28 -0800

    Rearranging the Explorer
    The locations of the files will change as well as the backend solution being updated to all the new locations of the files.

---
+++ SourceCode/SS.Backend/APIs/AccountManagement/Program.cs
@@ -0,0 +1,41 @@
+using SS.Backend.UserManagement;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+
+// Add services to the container.
+//builder.Services.AddTransient<ISqlDAO, SealedSqlDAO>();
+builder.Services.AddTransient<IAccountRecovery, AccountRecoveryNoInj>();
+
+// Learn more about configuring Swagger/OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder => builder.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowSpecificOrigin");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ SourceCode/SS.Backend/APIs/accountRecoveryAPI/Program.cs
@@ -0,0 +1,48 @@
+using SS.Backend.UserManagement;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IAccountRecovery, AccountRecoveryNoInj>();
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder => builder.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+    
+    
+}
+
+
+
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowSpecificOrigin");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
