ef414090f12e7c76166b69d502d446f984c26d2a
carinegordillo <carinegordillo@gmail.com>
2024-05-10 14:13:09 -0700

    status updater fixed

--- SourceCode/SS.Backend/SS.Backend.ReservationManagement/Implementations/ReservationReadService.cs
+++ SourceCode/SS.Backend/SS.Backend.ReservationManagement/Implementations/ReservationReadService.cs
@@ -13,6 +13,7 @@ namespace SS.Backend.ReservationManagement{
     public class ReservationReadService : IReservationReadService
     {
         private IReservationManagementRepository _reservationManagementRepository;
+    
 
         public ReservationReadService(IReservationManagementRepository reservationManagementRepository)
         {
@@ -20,7 +21,13 @@ namespace SS.Backend.ReservationManagement{
         }
 
         public async Task<Response> GetAllUserReservations(string tableName, string userHash){
+
             Response response = new Response();
+            Response updateResponse = new Response();
+
+            IReservationStatusUpdater _reservationStatusUpdater = new ReservationStatusUpdater(_reservationManagementRepository);
+
+            updateResponse = await _reservationStatusUpdater.UpdateReservtionStatuses(tableName, "UpdateReservtaionStatusesPROD");
 
             response = await _reservationManagementRepository.ReadReservationsTable("userHash", userHash, tableName);
 
