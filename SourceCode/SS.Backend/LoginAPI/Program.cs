c395a8731c0d653f66333188fa681dab75d40c2c
kaykayale <kaykayale@hotmail.com>
2024-05-10 20:03:13 -0700

    login api config

--- SourceCode/SS.Backend/LoginAPI/Program.cs
+++ SourceCode/SS.Backend/LoginAPI/Program.cs
@@ -5,6 +5,7 @@ using SS.Backend.Services.LoggingService;
 using SS.Backend.SharedNamespace;
 using SS.Backend.Services.ArchivingService ;
 using System.Text;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -74,19 +75,31 @@ app.Lifetime.ApplicationStopping.Register(() => {
 //     app.UseSwaggerUI();
 // }
 
+// get localhost cofig file path
+var corsConfigFilePath = Path.Combine(projectRootDirectory, "Configs", "originsConfig.json");
+string allowedOrigin= "coudl not connect to config file";
+
+if (File.Exists(corsConfigFilePath))
+{
+    string configJson = File.ReadAllText(corsConfigFilePath);
+    
+    JsonDocument doc = JsonDocument.Parse(configJson);
+    JsonElement root = doc.RootElement.GetProperty("Origin");
+    allowedOrigin = root.GetProperty("CorsAllowedOrigin").GetString() ?? "NA";
+}
+
+Console.WriteLine("Cors Allowed Origin: ");
+Console.WriteLine(allowedOrigin);
 app.Use(async (context, next) =>
 {
-    // Get the origin header from the request
     var origin = context.Request.Headers[HeaderNames.Origin].ToString();
-
-    var allowedOrigins = new[] { "http://localhost:3000" };
-
+    var allowedOrigins = new[] {allowedOrigin};
 
     if (!string.IsNullOrEmpty(origin) && allowedOrigins.Contains(origin))
     {
-        context.Response.Headers.Append(HeaderNames.AccessControlAllowOrigin, origin);
-        context.Response.Headers.Append(HeaderNames.AccessControlAllowMethods, "GET, POST, OPTIONS");
-        context.Response.Headers.Append(HeaderNames.AccessControlAllowHeaders, "Content-Type, Accept");
+        context.Response.Headers.Append("Access-Control-Allow-Origin", origin);
+        context.Response.Headers.Append("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+        context.Response.Headers.Append("Access-Control-Allow-Headers", "Content-Type, Accept");
     }
     if (context.Request.Method == "OPTIONS")
     {
