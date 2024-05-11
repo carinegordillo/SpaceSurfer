e2e76af35438bdb015897b81cc4250aea14a944c
kaykayale <kaykayale@hotmail.com>
2024-05-11 16:54:50 -0700

    added put option to cors

--- SourceCode/SS.Backend/TaskManagerHubAPI/Program.cs
+++ SourceCode/SS.Backend/TaskManagerHubAPI/Program.cs
@@ -92,7 +92,7 @@ app.Use(async (context, next) =>
     if (!string.IsNullOrEmpty(origin) && allowedOrigins.Contains(origin))
     {
         context.Response.Headers.Append("Access-Control-Allow-Origin", origin);
-        context.Response.Headers.Append("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+        context.Response.Headers.Append("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS");
         context.Response.Headers.Append("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
         context.Response.Headers.Append("Access-Control-Allow-Credentials", "true");
     }
