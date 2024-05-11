cdd38f56b165fd23f105f5c0ee31ff5e2b9aafe6
carinegordillo <carinegordillo@gmail.com>
2024-05-11 09:14:36 -0700

    new api

--- SourceCode/SS.Backend/adminAccountRecoveryAPI/Program.cs
+++ SourceCode/SS.Backend/adminAccountRecoveryAPI/Program.cs
@@ -1,3 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using SS.Backend.UserManagement;
+using SS.Backend.DataAccess;
+
+using SS.Backend.SharedNamespace;
+using SS.Backend.Services.LoggingService;
+using System.Text.Json;
+using Microsoft.Net.Http.Headers;
+
+
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,21 +19,85 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
+
+var baseDirectory = AppContext.BaseDirectory;
+var projectRootDirectory = Path.GetFullPath(Path.Combine(baseDirectory, "../../../../../"));
+var configFilePath = Path.Combine(projectRootDirectory, "Configs", "config.local.txt");
+
+
+//Dao Setup
+builder.Services.AddTransient<ConfigService>(provider =>new ConfigService(configFilePath));
+builder.Services.AddTransient<ISqlDAO, SqlDAO>();
+builder.Services.AddTransient<CustomSqlCommandBuilder>();
+
+
+builder.Services.AddTransient<GenOTP>();
+builder.Services.AddTransient<Hashing>();
+builder.Services.AddTransient<Response>();
+builder.Services.AddTransient<LogEntry>();
+builder.Services.AddTransient<ILogTarget, SqlLogTarget>();
+builder.Services.AddTransient<SS.Backend.Services.LoggingService.ILogger,Logger>();
+builder.Services.AddTransient<Logger>();
+builder.Services.AddTransient<SqlDAO>();
+
+builder.Services.AddTransient<CustomSqlCommandBuilder>();
+builder.Services.AddTransient<IUserManagementDao, UserManagementDao>();
+builder.Services.AddTransient<IAccountRecoveryModifier, AccountRecoveryModifier>();
+builder.Services.AddTransient<IProfileModifier, ProfileModifier>();
+builder.Services.AddTransient<IAccountRecovery, AccountRecovery>();
+builder.Services.AddTransient<IAccountDisabler, AccountDisabler>();
+
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// get localhost cofig file path
+var corsConfigFilePath = Path.Combine(projectRootDirectory, "Configs", "originsConfig.json");
+string allowedOrigin= "coudl not connect to config file";
+
+if (File.Exists(corsConfigFilePath))
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    string configJson = File.ReadAllText(corsConfigFilePath);
+    
+    JsonDocument doc = JsonDocument.Parse(configJson);
+    JsonElement root = doc.RootElement.GetProperty("Origin");
+    allowedOrigin = root.GetProperty("CorsAllowedOrigin").GetString() ?? "NA";
 }
 
-app.UseHttpsRedirection();
+Console.WriteLine("Cors Allowed Origin: ");
+Console.WriteLine(allowedOrigin);
+app.Use(async (context, next) =>
+{
+    var origin = context.Request.Headers[HeaderNames.Origin].ToString();
 
-app.UseAuthorization();
+    Console.WriteLine("IN HERERREEER ");
+    Console.WriteLine(allowedOrigin);
+
+    var allowedOrigins = new[] {allowedOrigin};
+
+    if (!string.IsNullOrEmpty(origin) && allowedOrigins.Contains(origin))
+    {
+        context.Response.Headers.Append("Access-Control-Allow-Origin", origin);
+        context.Response.Headers.Append("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS");
+        context.Response.Headers.Append("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        context.Response.Headers.Append("Access-Control-Allow-Credentials", "true");
+    }
+    if (context.Request.Method == "OPTIONS")
+    {
+        context.Response.StatusCode = StatusCodes.Status200OK;
+        await context.Response.CompleteAsync();
+    }
+    else
+    {
+        await next();
+    }
+});
+
+
+
 
 app.MapControllers();
 
 app.Run();
+
