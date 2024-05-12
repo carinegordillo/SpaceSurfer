e7e2d01b746184a228fadb19f3b02397369ba538
sanarahtos <77854933+sanarahtos@users.noreply.github.com>
2024-05-12 16:25:25 -0700

    added usedFeature function to confirmedreservations.js

--- SourceCode/SS.Frontend/Login/confirmedReservations.js
+++ SourceCode/SS.Frontend/Login/confirmedReservations.js
@@ -422,6 +422,7 @@ function openDialogCancel(username, reservationID) {
     yesBtn.innerText = 'Yes';
     yesBtn.onclick = () => {
         cancelConfirmation(username, reservationID);
+        fetchInsertUsedFeature('Confirmations');
         leaveModal();
     };
 
@@ -471,6 +472,7 @@ function openDialogDelete(reservationID) {
     yesBtn.innerText = 'Yes';
     yesBtn.onclick = () => {
         deleteConfirmation(reservationID);
+        fetchInsertUsedFeature('Confirmations');
         leaveModal();
     };
 
@@ -508,8 +510,66 @@ function leaveModal() {
     }
 }
 
+// Function to insert the used feature after every use
+async function fetchInsertUsedFeature(featureName) {
+    // Retrieve tokens from sessionStorage
+    var accessToken = sessionStorage.getItem('accessToken');
+    var idToken = sessionStorage.getItem('idToken');
+    var parsedIdToken = JSON.parse(idToken);
+    var username = parsedIdToken.Username;
+
+    // Check if tokens are present
+    if (!idToken) {
+        console.error('ID token not found.');
+        return;
+    }
+
+    if (!username) {
+        console.error('Username not found in token.');
+        return;
+    }
+
+    if (!accessToken) {
+        console.error('Access token not found.');
+        return;
+    }
+    if (!appConfig) {
+        console.error('Configuration is not loaded!');
+        return;
+    }
+    const SOurl = appConfig.api.SystemObservability;
+    // Construct the URL with the provided time span
+    const url = `${SOurl}/api/v1/SystemObservability/UsedFeatureInsertion?FeatureName=${featureName}`;
+
+    try {
+        // Fetch data from the API
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ` + accessToken,
+                'Content-Type': "application/json",
+            }
+        });
+
+        // Parse and return the response data
+        const data = await response.json();
+
+        // Check if there's any error in the response
+        if (data.hasError) {
+            throw new Error(`Error inserting the feature use: ${response.status}`);
+        }
+
+        return data;
+    }
+    catch (error) {
+        // Catch and log any errors that occur during the process
+        throw new Error(`Caught an error inserting the feature use: ${error.message}`);
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('initConfirmationsButton').addEventListener('click', () => {
         listConfirmations();
+        fetchInsertUsedFeature('Confirmations');
     });
 });
